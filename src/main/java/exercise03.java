a022764fc4c64155190e3d720fe7ed7ba719d99a
Rachel Cameron <rachleecameron@gmail.com>
2021-09-13 14:33:21 -0400

    Initial commit

---
+++ src/main/java/exercise03.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Rachel Cameron
+ */
+
+import java.util.Scanner;
+
+public class exercise03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the quote?");
+        String quote = input.nextLine();
+        System.out.println("Who said it?");
+        String author = input.nextLine();
+        System.out.printf("%s says, \"%s\"", author, quote);
+    }
+}
